7dde5387b5ff30a5087a6ae6e4fa8b70c00bcfa4
Jaden Lemmon <jaden@jadenlemmon.com>
2019-09-12 10:32:23 -0500

    Add update branch script

--- scripts/hg.sh
+++ scripts/hg.sh
@@ -4,10 +4,11 @@
 usage() {
   echo "hg [CMD] [ARGS]
 
- Git util commands
+ Handy Git Commands
 
  Commands:
  cm                 Clears all local branches merged into checkout out branch
+ u                  Updates current branch in repo from origin and installs dependencies
 "
 }
 
@@ -30,6 +31,15 @@ BRANCH=$(git symbolic-ref --short HEAD)
 if [ "$1" == "cm" ]; then
     git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
     echo "Cleared local branches merged into $BRANCH"
+elif [ "$1" == "u" ]; then
+    # update repo
+    git pull
+    
+    # install js dependencies
+    test -e ./yarn.lock && yarn || npm i
+    
+    # install php dependencies
+    test -e ./composer.json && composer install
 else
     echo "Must provide a command see --help for a list of commands"
 fi
